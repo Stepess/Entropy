ff3b4ef4c6cd3dc6c8d751d10fadf1648e46fec5
Stepess <stepanersh@gmail.com>
2018-02-22 21:15:07 +0200

    second commit

--- src/entropy/Entropy.java
+++ src/entropy/Entropy.java
@@ -1,6 +1,7 @@
 package entropy;
 
 import java.io.*;
+import java.util.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,32 +20,46 @@ public class Entropy {
     /**
      * @param args the command line arguments
      */
+    static String text="";
     static final String PATH = "D:\\\\Programming\\Crypt\\entropy\\text\\input.txt";
     public static void main(String[] args) throws Exception {
+        
         try{
-            Reader.readFile(PATH);
+            text=Reader.readFile(PATH);
         }
         catch(IOException ex){
             System.out.print(ex.getMessage());
+        }
+        int[] count = new int[32];
+        for (char c: text.toCharArray()){
+            count[((int)c - 1072)] +=1; 
+        }   
+        for (int i: count){
+            System.out.println(i);
         }
     }
     
 }
 
 class Reader {
-    public static String readFile(String path) throws Exception{
+    public static String readFile(String path) throws IOException{
         String res = "";
-        try(FileReader reader = new FileReader(path))
-        {
-            int c;
-            while((c=reader.read())!=-1){
-                System.out.print((char)c);
-            }
-            reader.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path),"CP1251"));
+        StringBuilder sb = new StringBuilder();
+        String s;
+        while((s = br.readLine()) != null) {
+            s=s.toLowerCase();
+            //s=s.replaceAll("\\w+", "");
+            s=s.replaceAll("\\w+|\\!|\\,|\\.|\\?|\\s", "");
+            sb.append(s);
         }
-        catch(IOException ex){
-            System.out.print(ex.getMessage());
-        }
+        res = sb.toString();
+        //System.out.println(s);
+        br.close();
+        
+        
         return res;
     }
 }
+
+
